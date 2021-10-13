30bd6749ee240925b71315a9f983cb63e69de4ec
salvospe <salvospe88@gmail.com>
2021-10-13 14:59:15 +0100

    V14
    Main menu, options menu

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+
+
+    void Start()
+    {
+        //getting available resolutions
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        //converting resolutions to strings
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i=0;i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if((resolutions[i].width == Screen.currentResolution.width)&& 
+                (resolutions[i].height == Screen.currentResolution.height))
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        //add options list to resolutionDropdown
+        resolutionDropdown.AddOptions(options);
+
+        //show current resolution
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreenbool (bool isFullscreen)
+    {
+        Debug.Log(isFullscreen);
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
