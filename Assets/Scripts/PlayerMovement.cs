5025a21a884fbbe9ea748cd94fa195259d76768e
salvospe <salvospe88@gmail.com>
2021-10-29 18:23:13 +0100

    V40
    Levels 1 - 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,13 +13,13 @@ public class PlayerMovement : MonoBehaviour
     // "Fixed"Update because we are dealing with physics
     void FixedUpdate()
     {
-        player.AddForce(0, 0, forwardForce * Time.deltaTime);
+        //player.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        //if (Input.GetKey("w"))
-        //    player.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        if (Input.GetKey("w"))
+            player.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
 
-        //if (Input.GetKey("s"))
-        //    player.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        if (Input.GetKey("s"))
+            player.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
 
         if (Input.GetKey("d"))
             player.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
